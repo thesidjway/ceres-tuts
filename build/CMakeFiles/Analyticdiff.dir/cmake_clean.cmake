c7d94d48532ec46aa56a76baf4d4c1e75883d35d
Siddharth Jha <sidchicharito@gmail.com>
2017-02-19 10:58:07 +0530

    Added analytic diff

---
+++ build/CMakeFiles/Analyticdiff.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Analyticdiff.dir/src/Analytic_diff.cpp.o"
+  "Analyticdiff.pdb"
+  "Analyticdiff"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Analyticdiff.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- build/CMakeFiles/Makefile.cmake
+++ build/CMakeFiles/Makefile.cmake
@@ -55,6 +55,7 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
+  "CMakeFiles/Analyticdiff.dir/DependInfo.cmake"
   "CMakeFiles/Numericdiff.dir/DependInfo.cmake"
   "CMakeFiles/Helloworld.dir/DependInfo.cmake"
   )
