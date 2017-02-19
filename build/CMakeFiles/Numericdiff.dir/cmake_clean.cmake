f5eef906e21f6844a949ef33be66f4acf6f9287f
Siddharth Jha <sidchicharito@gmail.com>
2017-02-19 10:54:23 +0530

    Added Numeric Differentiation

---
+++ build/CMakeFiles/Numericdiff.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Numericdiff.dir/src/Numeric_diff.cpp.o"
+  "Numericdiff.pdb"
+  "Numericdiff"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Numericdiff.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
